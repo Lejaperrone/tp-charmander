f736f3813211027eb486460a3f450c9d79d61001
TomasRS <tomas_rs@hotmail.com>
2016-09-03 18:35:56 -0300

    Agrego logueo para configuracion de Entrenador

--- Entrenador/src/Entrenador.c
+++ Entrenador/src/Entrenador.c
@@ -8,12 +8,15 @@
 #include <stdbool.h>
 #include "socketLib.h"
 #include <commons/config.h>
+#include <commons/log.h>
 
-#define IP_MAPA "127.0.0.1" //Lo deberia buscar del metadata del primer mapa de su hoja de viaje
-#define PUERTO_MAPA "6667"  // En realidad lo deberia buscar del metadata del primer mapa de su hoja de viaje
+#define IP "127.0.0.1" //Lo deberia buscar del metadata del primer mapa de su hoja de viaje
+#define PUERTO "6667"  // En realidad lo deberia buscar del metadata del primer mapa de su hoja de viaje
 #define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
 
 void leerConfiguracion();
+t_log* crearArchivoLog();
+void loguearConfiguracion();
 
 typedef struct{
 	char* nombre;
@@ -23,14 +26,21 @@ typedef struct{
 	//Falta hoja de viaje y los objetivos
 }t_entrenador;
 
+t_log* archivoLog;
+
 int main(){
 
-	leerConfiguracion();
+	t_entrenador* entrenador = (t_entrenador*) malloc(sizeof(t_entrenador));
+	leerConfiguracion(entrenador);
+
+	archivoLog = crearArchivoLog();
+	log_info(archivoLog,"Cliente levantado.\n");
+	loguearConfiguracion(archivoLog, entrenador);
 
 	int serverMapa;
 
 	printf("Conectandose al servidor...\n");
-	create_socketClient(&serverMapa, IP_MAPA, PUERTO_MAPA);
+	create_socketClient(&serverMapa, IP, PUERTO);
 	printf("Conectado al servidor. Ya puede enviar mensajes. Escriba 'exit' para salir\n");
 
 //------------Envio de mensajes al servidor------------
@@ -48,24 +58,41 @@ int main(){
 }
 
 
-void leerConfiguracion(){
+void leerConfiguracion(t_entrenador* entrenador){
 
 //¿Como hacemos que cargue el metadata del entrenador que corresponde? Porque en este caso cargaria solo el de Red
 	t_config* config = config_create("../../PokedexConfig/Entrenadores/Red/metadata");
-	t_entrenador* entrenador = (t_entrenador*) malloc(sizeof(t_entrenador));
 	entrenador->nombre = config_get_string_value(config, "nombre");
 	entrenador->simbolo = config_get_string_value(config, "simbolo");
 	entrenador->vidas = config_get_int_value(config, "vidas");
 	entrenador->reintentos = config_get_int_value(config, "reintentos");
 
+}
 
-//Muestra de que se asignaron bien los datos del archivo metadata. (Para el checkpoint habria que borrar esto)
-	printf("---------------Mi configuracion---------------\n");
-	printf("Nombre: %s\n", entrenador->nombre);
-	printf("Simbolo: %s\n", entrenador->simbolo);
-	printf("Vidas: %d\n", entrenador->vidas);
-	printf("Reintentos: %d\n", entrenador->reintentos);
-	printf("----------------------------------------------\n");
+t_log* crearArchivoLog() {
+
+	remove("logsEntrenador");
+
+	t_log* logs = log_create("logsEntrenador", "EntrenadorLog", 0, LOG_LEVEL_TRACE);
+
+	if (logs == NULL) {
+		puts("No se pudo generar el archivo de logueo.\n");
+		return NULL;
+	}
+
+
+	log_info(logs, "ARCHIVO DE LOGUEO INICIALIZADO");
+
+	return logs;
+}
+
+void loguearConfiguracion(t_log* archivoLogs, t_entrenador* entrenador){
+	log_info(archivoLogs, "CONFIGURACION DEL METADATA");
+	log_info(archivoLogs, "Nombre: %s", entrenador->nombre);
+	log_info(archivoLogs, "Simbolo: %s", entrenador->simbolo);
+	log_info(archivoLogs, "Vidas: %d", entrenador->vidas);
+	log_info(archivoLogs, "Reintentos %d", entrenador->reintentos);
+	//Faltan los que son de array
 }
 
 
