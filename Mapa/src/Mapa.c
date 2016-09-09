a28aa66443fc2ccd49350b61e2b568110df38102
jbmild <jbmild@gmail.com>
2016-09-09 13:19:51 -0300

    bug cambio de paths

--- Mapa/src/Mapa.c
+++ Mapa/src/Mapa.c
@@ -146,7 +146,7 @@ int main(){
 void leerConfiguracion(t_mapa* mapa){
 
 //¿Como hacemos que cargue el metadata del mapa que corresponde? Porque en este caso cargaria solo el de Red
-	t_config* config = config_create("../../PokedexConfig/Mapas/Ciudad Paleta/metadata");
+	t_config* config = config_create("../../PokedexConfig/Mapas/PuebloPaleta/metadata");
 	mapa->tiempoChequeoDeadlock = config_get_int_value(config, "TiempoChequeoDeadlock");
 	mapa->batalla = config_get_int_value(config, "Batalla");
 	mapa->algoritmo = config_get_string_value(config, "algoritmo");
