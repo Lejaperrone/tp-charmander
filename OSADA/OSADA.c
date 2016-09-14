b98f200c34f3292f2e309eb680ac0ff039511197
Francisco Patalagoity <fpatalagoity@hotmail.com>
2016-09-14 16:51:02 -0300

    commit para pullear

--- OSADA/OSADA.c
+++ OSADA/OSADA.c
@@ -46,30 +46,45 @@ _Static_assert( sizeof(osada_file) == (sizeof(osada_block) / 2.0), "osada_file s
 #pragma pack(pop)
 
 int main (){
-	FILE* arch;
-	char id[7];
-	char version;
-	arch=fopen("miArchivo.bin","r");
-	//agregar por si falla el abrir archivo
-	char* datos=malloc(sizeof(char*));
-	fread(datos,OSADA_BLOCK_SIZE,1,arch);
 	int i;
-	for (i=0;i<OSADA_BLOCK_SIZE;i++){
+	FILE* arch;
+	arch=fopen("miArchivo.bin","r");
+	fseek(arch, 0L, SEEK_SET);
+	long tamArch;
+	tamArch=ftell(arch);
+	printf("miArchivo.bin ocupa %li bytes\n", tamArch);
+	char* datos=malloc(sizeof(char*));
+	fread(datos, OSADA_BLOCK_SIZE, 1, arch);
+	//agregar por si falla el abrir archivo
+	int j=0;
+	for (i=0;i<7;i++){
 		header.magic_number[i]=datos[i];
-
-	printf("Lo que lei es: %s\n",header.magic_number);
-		int j;
-		for(j=0;j<1;j++){
-			header.version[j]=datos[i];
-		}
-	printf("la version es: %d\n",header.version);
-		for (j=0;j<4;j++){
-		char* bqFS=malloc(sizeof(char*));
-		bqFS[j]=datos[i];
-		int bloquesFS;
-		bloquesFS=atoi(bqFS);
-		}
+		j++;
 	}
-	printf("tiene %d bloques el FS\n",header.fs_blocks);
+	printf("Identificador: %s\n",header.magic_number);
+	//--------------------------------------------------------------
+	for(i=0;i<1;i++){
+			header.version=datos[j];
+			j++;
+		}
+	printf("Version: %d\n",header.version);
+	char bq[4];
+	for (i=0;i<4;i++){
+			bq[i]=datos[j];
+			printf("leo bq: %c\n",bq[i]);
+			printf("j vale %d\n",j);
+			printf("leo datos: %c\n",datos[j]);
+			j++;
+	}
+	header.fs_blocks=atoi(bq);
+	printf("FS tiene %d bloques\n",header.fs_blocks);
+	//header.fs_blocks=atoi(bq);
+	printf("Tamanio del FS: %d\n",header.fs_blocks);
+	/*header.bitmap_blocks=header.fs_blocks/8/OSADA_BLOCK_SIZE;
+	printf("Tamanio del bitmap: %d\n",header.bitmap_blocks);
+	header.allocations_table_offset=1+header.bitmap_blocks+1024;
+	printf("Inicio Tabla Asignaciones: %d\n",header.allocations_table_offset);
+	header.data_blocks=header.fs_blocks-1-header.bitmap_blocks-1024-header.allocations_table_offset;
+	printf("Tamanio de datos [bloques]: %d\n",header.data_blocks);*/
 	return 1;
 }
