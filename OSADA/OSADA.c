1c982edf77db716f774a5e330260fcbd335826a8
jbmild <jbmild@gmail.com>
2016-09-14 17:40:40 -0300

    osada header funcionando!!

--- OSADA/OSADA.c
+++ OSADA/OSADA.c
@@ -48,12 +48,8 @@ _Static_assert( sizeof(osada_file) == (sizeof(osada_block) / 2.0), "osada_file s
 #pragma pack(pop)
 
 int main (){
-	int i;
 	FILE* arch;
 	arch=fopen("miArchivo.bin","r");
-	//fseek(arch, 0L, SEEK_SET);
-	long tamArch;
-	tamArch=ftell(arch);
 
 	struct stat sbuf;
 	if (stat("miArchivo.bin", &sbuf) == -1) {
@@ -61,40 +57,13 @@ int main (){
 		exit(1);
 	}
 
-
 	printf("miArchivo.bin ocupa %li bytes\n", sbuf.st_size-1);
-	char* datos=malloc(sizeof(char*));
-	fread(datos, OSADA_BLOCK_SIZE*8, 1, arch);
-	//agregar por si falla el abrir archivo
-	int j=0;
-	for (i=0;i<7;i++){
-		header.magic_number[i]=datos[i];
-		j++;
-	}
+
+	fread(&header,sizeof(osada_header),1,arch);
+
 	printf("Identificador: %s\n",header.magic_number);
-	//--------------------------------------------------------------
-	for(i=0;i<1;i++){
-			header.version=datos[j];
-			j++;
-		}
 	printf("Version: %d\n",header.version);
-	char bq[4];
-	for (i=0;i<4;i++){
-			bq[i]=datos[j];
-			printf("leo bq: %c\n",bq[i]);
-			printf("j vale %d\n",j);
-			printf("leo datos: %c\n",datos[j]);
-			j++;
-	}
-	header.fs_blocks=atoi(bq);
 	printf("FS tiene %d bloques\n",header.fs_blocks);
-	//header.fs_blocks=atoi(bq);
-	printf("Tamanio del FS: %d\n",header.fs_blocks);
-	/*header.bitmap_blocks=header.fs_blocks/8/OSADA_BLOCK_SIZE;
-	printf("Tamanio del bitmap: %d\n",header.bitmap_blocks);
-	header.allocations_table_offset=1+header.bitmap_blocks+1024;
-	printf("Inicio Tabla Asignaciones: %d\n",header.allocations_table_offset);
-	header.data_blocks=header.fs_blocks-1-header.bitmap_blocks-1024-header.allocations_table_offset;
-	printf("Tamanio de datos [bloques]: %d\n",header.data_blocks);*/
+
 	return 1;
 }
