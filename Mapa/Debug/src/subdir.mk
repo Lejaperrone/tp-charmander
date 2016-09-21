bbf2ff7073cebaa09f659e714745e0bf9ad738ab
Francisco Patalagoity <fpatalagoity@hotmail.com>
2016-09-21 18:21:35 -0300

    build post pull

--- Mapa/Debug/src/subdir.mk
+++ Mapa/Debug/src/subdir.mk
@@ -5,18 +5,15 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../src/Mapa.c \
-../src/socketLib.c \
-../src/test.c 
+../src/socketLib.c 
 
 OBJS += \
 ./src/Mapa.o \
-./src/socketLib.o \
-./src/test.o 
+./src/socketLib.o 
 
 C_DEPS += \
 ./src/Mapa.d \
-./src/socketLib.d \
-./src/test.d 
+./src/socketLib.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
