cc59ad3590234e1e11b3e0dd6a9736dd4923ef85
Francisco Patalagoity <fpatalagoity@hotmail.com>
2016-10-04 16:08:02 -0300

    refactorizo funciones de deadlock para entender mejor que hace cada una

--- Mapa/src/threads/deadlock.c
+++ Mapa/src/threads/deadlock.c
@@ -28,9 +28,11 @@ bool noHayPokemonsDisponiblesPara(t_entrenador* unE){
 	for(i=0;i<sizePokenests;i++){
 	t_pokenest* unaPokenest=malloc(sizeof(t_pokenest));
 			 unaPokenest=list_get(mapa->pokeNests,i);
-
 			 pokemonsDisponibles=list_filter(unaPokenest->pokemons,(void*)estaDisponible (unPoke));
+			 free(unaPokenest);
 	}
+	 free(unPoke);
+//	 free(pokemonsDisponibles);
 	 return (list_size(pokemonsDisponibles)<0);
 }
 
@@ -38,27 +40,48 @@ bool poseePokemonSolicitado (t_entrenador* unE){
 
 	return (list_count_satisfying(unE->pokemons,(void*)estaDisponible)>0);
 }
+void juntarTodosLosEntrenadores(t_list* posiblesEntrenadoresEnDeadlock) {
+	sizeTrainersOnMap=list_size(entrenadoresListos)+list_size(entrenadoresBloqueados);
+			list_add_all(posiblesEntrenadoresEnDeadlock,entrenadoresListos);
+			list_add_all(posiblesEntrenadoresEnDeadlock,entrenadoresBloqueados);
+
+}
+t_list* analizarDeadlock (){
+	int i;
+	for (i=0;i<sizeTrainersOnMap;i++){
+				t_entrenador* posibleEntrenadorEnDeadlock=malloc(sizeof(t_entrenador));
+				posibleEntrenadorEnDeadlock=(t_entrenador*)list_get(posiblesEntrenadoresEnDeadlock,i);
+				if (noHayPokemonsDisponiblesPara(posibleEntrenadorEnDeadlock) && poseePokemonSolicitado(posibleEntrenadorEnDeadlock) ){
+					list_add(entrenadoresEnDeadlock,posibleEntrenadorEnDeadlock);
+				}
+			}
+	return entrenadoresEnDeadlock;
+}
+bool hayEntrenadoresEnDeadlock(t_list* entrenadores){
+	return list_size(entrenadoresEnDeadlock)>0;
+}
+bool batallaActivada(){
+	return mapa->batalla==1;
+}
 
 void* deadlock(void* arg){
 	log_trace(archivoLog, "Deadldock - Arranca");
 	while (1){
+		t_list* entrenadoresParaAnalizar=malloc(sizeof(t_entrenador));
+		t_list* entrenadoresAnalizados=malloc(sizeof(t_entrenador));
 		sleep(mapa->retardo);
-		int i;
-		sizeTrainersOnMap=list_size(entrenadoresListos);
-				//+list_size(t_entrenadoresBloqueados);
-		t_list* posiblesEntrenadoresEnDeadlock=malloc(sizeof(t_list));
-		list_add_all(posiblesEntrenadoresEnDeadlock,entrenadoresListos);
-		list_add_all(posiblesEntrenadoresEnDeadlock,entrenadoresBloqueados);
-		for (i=0;i<sizeTrainersOnMap;i++){
-			t_entrenador* posibleEntrenadorEnDeadlock=malloc(sizeof(t_entrenador));
-			posibleEntrenadorEnDeadlock=(t_entrenador*)list_get(posiblesEntrenadoresEnDeadlock,i);
-			if (noHayPokemonsDisponiblesPara(posibleEntrenadorEnDeadlock) && poseePokemonSolicitado(posibleEntrenadorEnDeadlock) ){
-				list_add(entrenadoresEnDeadlock,posibleEntrenadorEnDeadlock);
-			}
+		juntarTodosLosEntrenadores(entrenadoresParaAnalizar);
+		entrenadoresAnalizados=analizarDeadlock(entrenadoresParaAnalizar);
+		if (hayEntrenadoresEnDeadlock(entrenadoresParaAnalizar)){
+			if (batallaActivada()){
+				//BATALLA PKMN
+		}else{
+		log_info(archivoLog,"Hay DEADLOCK pero no hay batalla pokemon configurada");
 		}
-		if (list_size(entrenadoresEnDeadlock)>0&&mapa->batalla==1){
-
-		}
+			free(posiblesEntrenadoresEnDeadlock);
+			free(entrenadoresParaAnalizar);
+				free(entrenadoresAnalizados);
+	}
 	}
 
 	return arg;
