d61d3ea596746fc0a112415eefd6a9dcbb48c379
Francisco Patalagoity <fpatalagoity@hotmail.com>
2016-10-06 02:51:19 -0300

    creacion de la matriz de asignados

--- Mapa/src/threads/deadlock.c
+++ Mapa/src/threads/deadlock.c
@@ -7,11 +7,14 @@
 
 #include "../commons/structures.h"
 #include <pkmn/battle.h>
+#include <unistd.h>
 
 
 t_list* entrenadoresEnDeadlock;
 t_list* posiblesEntrenadoresEnDeadlock;
 int sizeTrainersOnMap;
+int **mAsignacion;
+int **mSolicitud;
 
 //bool(*condition)(void*)
 
@@ -62,13 +65,63 @@ bool hayEntrenadoresEnDeadlock(t_list* entrenadores){
 bool batallaActivada(){
 	return mapa->batalla==1;
 }
+bool esEsePokemon(t_pokemon_custom* p1, t_pokemon_custom* p2){
+	return p1->id==p2->id;
+}
+bool loTieneAsignado(t_entrenador* e, t_list* p){
+	t_list* lpok=list_create();
+	list_add_all(lpok,e->pokemons);
+	return list_any_satisfy(lpok, (void*)esEsePokemon );
+}
+int asignarCantidadDePokemons (int entr, int poke){
+	t_entrenador* entrAsig=(t_entrenador*)malloc(sizeof(t_entrenador));
+	entrAsig=list_get(entrenadoresBloqueados,entr);
+	t_pokenest* pokenestAsig=(t_pokenest*)malloc(sizeof(t_pokenest));
+	pokenestAsig=(t_pokenest*)list_get(mapa->pokeNests,poke);
+
+	return list_count_satisfying(pokenestAsig->pokemons, (void*)loTieneAsignado);
+}
 
 void* deadlock(void* arg){
 	log_trace(archivoLog, "Deadldock - Arranca");
 	while (1){
+		log_info(archivoLog,"Hilo deadlock espera proxima vez de chequeo");
+		//sleep(0.1*(mapa->retardo));
+		sleep(2);
+		log_info(archivoLog,"Hilo deadlock comienza su deteccion");
+		int i,j;
+		if (list_size(entrenadoresBloqueados)==0){
+					log_info(archivoLog,"No hay entrenadores bloqueados aun");
+				}else{
+		//REFACTORIZAR TODO ESTE ELSE EN FUNCIONES MAS CHICAS
 		t_list* entrenadoresParaAnalizar=malloc(sizeof(t_entrenador));
 		t_list* entrenadoresAnalizados=malloc(sizeof(t_entrenador));
-		sleep(mapa->retardo);
+		log_info(archivoLog,"Se crea la matriz de asignacion de pokemons ");
+		mAsignacion=(int**)malloc(list_size(entrenadoresBloqueados)*sizeof(int*));
+		for (i=0;i<list_size(entrenadoresBloqueados);i++){
+			mAsignacion[i]=(int*)malloc(list_size(mapa->pokeNests)*sizeof(int));
+		}
+		log_info(archivoLog,"Comienzo a llenar la matriz ");
+
+		for (i=0;i<list_size(entrenadoresBloqueados);i++){
+			for (j=0;j<list_size(mapa->pokeNests);j++){
+			mAsignacion[i][j]=asignarCantidadDePokemons(i,j);
+			t_entrenador* e = (t_entrenador*)malloc(sizeof(t_entrenador));
+			e=list_get(entrenadoresBloqueados,i);
+			t_pokenest* p=(t_pokenest*)malloc(sizeof(t_pokenest));
+			p=list_get(mapa->pokeNests,j);
+			log_info(archivoLog,"%c tiene asignados %d %c",e->simbolo,mAsignacion[i][j],p->identificador );
+			}
+		}
+
+		//Logueo la matriz
+		for (i=0;i<list_size(entrenadoresBloqueados);i++){
+					for (j=0;j<list_size(mapa->pokeNests);j++){
+					log_info(archivoLog,"%d ",mAsignacion[i][j]);
+					}
+				}
+
+
 		juntarTodosLosEntrenadores(entrenadoresParaAnalizar);
 		entrenadoresAnalizados=analizarDeadlock(entrenadoresParaAnalizar);
 		if (hayEntrenadoresEnDeadlock(entrenadoresParaAnalizar)){
@@ -82,7 +135,7 @@ void* deadlock(void* arg){
 				free(entrenadoresAnalizados);
 	}
 	}
-
+	}
 	return arg;
 }
 
@@ -125,6 +178,7 @@ int esperaCircularPop(esperaCircular_t *c, uint8_t *data)
 }
 
 
+/*
 //falta asociar a la fila y columna con la estructura en mapas (t_list* entrenadoresBloqueados y el int cantidad de pokemonesAsignados)
 typedef struct matrizAsignada {
 	int fila_entrenadoresBloqueados;
@@ -134,3 +188,4 @@ typedef struct matrizAsignada {
 	for(int i=0; i<fila_entrenadoresBloqueados; i++);
 	matriz[i] = new int[columna_PokeAsignados];
 }t_matriz_asignada;
+*/
