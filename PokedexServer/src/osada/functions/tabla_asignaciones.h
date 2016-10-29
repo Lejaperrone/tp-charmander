3ef4032bee904e6e362ddf33338faf067bd34171
Francisco Patalagoity <fpatalagoity@hotmail.com>
2016-10-29 15:07:00 -0300

    agrego tabla de asignaciones .h

---
+++ PokedexServer/src/osada/functions/tabla_asignaciones.h
@@ -0,0 +1,15 @@
+/*
+ * tabla_asignaciones.h
+ *
+ *  Created on: 29/10/2016
+ *      Author: utnso
+ */
+
+#ifndef OSADA_FUNCTIONS_TABLA_ASIGNACIONES_H_
+#define OSADA_FUNCTIONS_TABLA_ASIGNACIONES_H_
+
+
+
+
+void actualizarTablaDeAsignaciones_porBaja(int* subindice, bool* hayMasBloques);
+#endif /* OSADA_FUNCTIONS_TABLA_ASIGNACIONES_H_ */
