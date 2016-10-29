309c48e12f03f0e601d4d2f59c89cdf092787c10
Francisco Patalagoity <fpatalagoity@hotmail.com>
2016-10-29 15:11:27 -0300

    agregro bitmap .c y .h

---
+++ PokedexServer/src/osada/functions/bitmap.c
@@ -0,0 +1,11 @@
+/*
+ * bitmap.c
+ *
+ *  Created on: 29/10/2016
+ *      Author: utnso
+ */
+
+
+void actualizarBitmap_porBaja (int subindice){
+	//osada_drive.bitmap
+}
---
+++ PokedexServer/src/osada/functions/bitmap.h
@@ -0,0 +1,13 @@
+/*
+ * bitmap.h
+ *
+ *  Created on: 29/10/2016
+ *      Author: utnso
+ */
+
+#ifndef OSADA_FUNCTIONS_BITMAP_H_
+#define OSADA_FUNCTIONS_BITMAP_H_
+
+void actualizarBitmap_porBaja (int subindice);
+
+#endif /* OSADA_FUNCTIONS_BITMAP_H_ */
