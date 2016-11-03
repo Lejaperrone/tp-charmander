cb3a6b071299f60fadf9a75f4d79898e1176912e
TomasRS <tomas_rs@hotmail.com>
2016-11-03 19:25:17 -0300

    Agrego mensajes para las funciones de fuse. Faltan completarlos.

--- PokedexCliente/src/functions/fuse.c
+++ PokedexCliente/src/functions/fuse.c
@@ -21,14 +21,25 @@
 #include <fcntl.h>
 #include "../commons/structures.h"
 
-int chamba_getattr (const char* path, struct stat* stbuf, struct fuse_file_info *fi){
-	int res = 0;
-	char * mensaje = string_new();
-	string_append(&mensaje, "GETAT");
+char* armarMensajeBasico(char* nombreFuncion, char* path){
+	char* mensaje = string_new();
+
+	string_append(&mensaje, nombreFuncion);
 	string_append(&mensaje, path);
 
+	return mensaje;
+}
+
+int chamba_getattr (const char* path, struct stat* stbuf, struct fuse_file_info *fi){
+	int res = 0;
+	char* mensaje = armarMensajeBasico("GETAT", path);
+
+//	char * mensaje = string_new();
+//	string_append(&mensaje, "GETAT");
+//	string_append(&mensaje, path);
+
 	if(send(pokedexServer, &mensaje, sizeof(mensaje), 0)){
-	char * resp;
+	char* resp;
 	recv(pokedexServer, &resp, 1024, 0);
 	}
 	memset(stbuf, 0, sizeof(struct stat));
@@ -52,13 +63,16 @@ int chamba_readdir(const char* path, void *buf, fuse_fill_dir_t filler, off_t of
 		(void) offset;
 		(void) fi;
 
-		char * mensaje = string_new();
-		string_append(&mensaje, "READD");
-		string_append(&mensaje, path);
+		char* mensaje = armarMensajeBasico("READD", path);
+		//falta agregarle el offset y algo mas al mensaje?
+
+//		char * mensaje = string_new();
+//		string_append(&mensaje, "READD");
+//		string_append(&mensaje, path);
 
 		if(send(pokedexServer, &mensaje, sizeof(mensaje), 0)){
-				char * resp;
-				recv(pokedexServer, &resp, 1024, 0);
+			char* resp;
+			recv(pokedexServer, &resp, 1024, 0);
 	   }
 
 	   if (strcmp(path, "/") != 0)
@@ -72,62 +86,94 @@ int chamba_readdir(const char* path, void *buf, fuse_fill_dir_t filler, off_t of
 }
 
 int chamba_open (const char * path, struct fuse_file_info * fi){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("OPENF", path);
+
 	return 0;
 }
 
 int chamba_read (const char * path, char * buffer, size_t size, off_t offset, struct fuse_file_info * fi){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("READF", path);
+	//falta agregarle el buffer, size y offset al mensaje?
+
 	return 0;
 }
 
 int chamba_create (const char * path, mode_t mode, struct fuse_file_info * fi){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("CREAT", path);
+	//falta agregarle el modo
+
 	return 0;
 }
 
 int chamba_truncate (const char * path, off_t offset){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("TRUNC", path);
+	//falta agregarle el offset
+
 	return 0;
 }
 
 int chamba_mkdir (const char * path, mode_t modo){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("MKDIR", path);
+	//falta agregarle el modo al mensaje?
+
 	return 0;
 }
 
 int chamba_rename (const char * path, const char * newPath){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("RENAM", path);
+	string_append(&mensaje, newPath);
+
 	return 0;
 }
 
 int chamba_unlink (const char * path){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("ULINK", path);
+
 	return 0;
 }
 
 int chamba_rmdir (const char * path){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("RMDIR", path);
+
 	return 0;
 }
 
 int chamba_write (const char * path, const char * buffer, size_t size, off_t offset, struct fuse_file_info * fi){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("WRITE", path);
+	//falta agregarle mas cosas al mensaje que vamos a mandar
+
 	return 0;
 }
 
 int chamba_statfs (const char * path, struct statvfs * stats){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("STATF", path);
+	//faltaria agregarle la estructura de stats?
+
 	return 0;
 }
 
 int chamba_release (const char * path, struct fuse_file_info * fi){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("RLEAS", path);
+
 	return 0;
 }
 
 int chamba_fallocate (const char * path, int amount, off_t sizeh, off_t sizef,  struct fuse_file_info * fi){
-	//TODO
+
+	char* mensaje = armarMensajeBasico("FALLO", path);
+	//falta agregarle el amount, sizeh y sizef al mensaje?
+
 	return 0;
 }
 
