2d0e6022d9a3eda7c340ad48bb4f163e8cd1b2fd
TomasRS <tomas_rs@hotmail.com>
2016-11-04 19:02:40 -0300

    Agrego funcion para reconocer el package mandado por el PokedexCliente. Falta completar.

--- PokedexServer/src/PokedexServer.c
+++ PokedexServer/src/PokedexServer.c
@@ -22,84 +22,130 @@ void solicitud(){
 
 }
 
+void identificarFuncionRecibida(char* package){
+
+	if(string_starts_with(package, "GETAT")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "READD")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "OPENF")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "READF")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "CREAT")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "TRUNC")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "MKDIR")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "RENAM")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "ULINK")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "RMDIR")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "WRITE")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "STATF")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "RLEAS")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+	if(string_starts_with(package, "FALOC")){
+		//conseguir el path e invocar la funcion correspondiente de osada
+	}
+}
+
 int main(){
 	printf("Inicio osada\n");
 	//Osada
-		osada_init("/home/utnso/git/tp-2016-2c-Chamba/osada.bin");
+	osada_init("/home/utnso/git/tp-2016-2c-Chamba/osada.bin");
 
 	//Inicializo socket para escuchar
-		struct sockaddr_in addr;
-		socklen_t addrlen = sizeof(addr);
+	struct sockaddr_in addr;
+	socklen_t addrlen = sizeof(addr);
 
-		int listeningSocket;
-		create_serverSocket(&listeningSocket, PUERTO);
+	int listeningSocket;
+	create_serverSocket(&listeningSocket, PUERTO);
 
 	//Inicializo el select
-		fd_set master;		// conjunto maestro de descriptores de fichero
-		fd_set read_fds;	// conjunto temporal de descriptores de fichero para select()
-		int fdmax;			// número máximo de descriptores de fichero
-		int newfd;			// descriptor de socket de nueva conexión aceptada
-		int i;
-		int nbytes;
-		char package[PACKAGESIZE];
+	fd_set master;		// conjunto maestro de descriptores de fichero
+	fd_set read_fds;	// conjunto temporal de descriptores de fichero para select()
+	int fdmax;			// número máximo de descriptores de fichero
+	int newfd;			// descriptor de socket de nueva conexión aceptada
+	int i;
+	int nbytes;
+	char package[PACKAGESIZE];
 
-		FD_ZERO(&master);					// borra los conjuntos maestro y temporal
-		FD_ZERO(&read_fds);
-		FD_SET(listeningSocket, &master);	// añadir listener al conjunto maestro
-		fdmax = listeningSocket; 			// seguir la pista del descriptor de fichero mayor, por ahora es este
+	FD_ZERO(&master);					// borra los conjuntos maestro y temporal
+	FD_ZERO(&read_fds);
+	FD_SET(listeningSocket, &master);	// añadir listener al conjunto maestro
+	fdmax = listeningSocket; 			// seguir la pista del descriptor de fichero mayor, por ahora es este
 
 	//Me mantengo en el bucle para asi poder procesar cambios en los sockets
-		while(1) {
-			//Copio los sockets y me fijo si alguno tiene cambios, si no hay itinero de vuelta
-				read_fds = master; // cópialo
-				if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
-					perror("select");
-					exit(1);
-				}
-
-			//Recorro los sockets con cambios
-				for(i = 0; i <= fdmax; i++) {
-					//Detecto si hay datos en un socket especifico
-					if (FD_ISSET(i, &read_fds)) {
-						//Si es el socket de escucha proceso el nuevo socket
-						if (i == listeningSocket) {
-							addrlen = sizeof(addr);
-							if ((newfd = accept(listeningSocket, (struct sockaddr*)&addr, &addrlen)) == -1){
-								perror("accept");
-							} else {
-								FD_SET(newfd, &master); // Añado el nuevo socket al  select
-								//Actualizo la cantidad
-								if (newfd > fdmax) {
-									fdmax = newfd;
-								}
-								printf("selectserver: new connection from %s on ""socket %d\n", inet_ntoa(addr.sin_addr),newfd);
-							}
-						} else {
-							//Si es un socket existente
-							if ((nbytes = recv(i, (void*)package, PACKAGESIZE, 0)) <= 0) {
-								//Si la conexion se cerro
-								if (nbytes == 0) {
-									printf("selectserver: socket %d hung up\n", i);
-								} else {
-									perror("recv");
-								}
-								close(i);
-								FD_CLR(i, &master); // eliminar del conjunto maestro
-
-							} else {
-								// tenemos datos de algún cliente
-								if (nbytes != 0){
-									printf("%s",package);
-
-								}
-							}
-						}
-						}
-				}
+	while(1) {
+		//Copio los sockets y me fijo si alguno tiene cambios, si no hay itinero de vuelta
+		read_fds = master; // cópialo
+		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+			perror("select");
+			exit(1);
 		}
 
+		//Recorro los sockets con cambios
+		for(i = 0; i <= fdmax; i++) {
+			//Detecto si hay datos en un socket especifico
+			if (FD_ISSET(i, &read_fds)) {
+				//Si es el socket de escucha proceso el nuevo socket
+				if (i == listeningSocket) {
+					addrlen = sizeof(addr);
+					if ((newfd = accept(listeningSocket, (struct sockaddr*)&addr, &addrlen)) == -1){
+						perror("accept");
+					} else {
+						FD_SET(newfd, &master); // Añado el nuevo socket al  select
+						//Actualizo la cantidad
+						if (newfd > fdmax) {
+							fdmax = newfd;
+						}
+						printf("selectserver: new connection from %s on ""socket %d\n", inet_ntoa(addr.sin_addr),newfd);
+					}
+				} else {
+					//Si es un socket existente
+					if ((nbytes = recv(i, (void*)package, PACKAGESIZE, 0)) <= 0) {
+						//Si la conexion se cerro
+						if (nbytes == 0) {
+							printf("selectserver: socket %d hung up\n", i);
+						} else {
+							perror("recv");
+						}
+						close(i);
+						FD_CLR(i, &master); // eliminar del conjunto maestro
+
+					} else {
+						// tenemos datos de algún cliente
+						if (nbytes != 0){
+							identificarFuncionRecibida(package);
+
+						}
+					}
+				}
+			}
+		}
+	}
+
 	//Libero memoria y termino ui
-		close(listeningSocket);
+	close(listeningSocket);
 
 	//Termino el mapa
 	return 0;
